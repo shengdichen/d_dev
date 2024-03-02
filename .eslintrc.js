5fb6efbe38eaa30e7d129ded24169a5d9946ae47
shc <sh@c>
2024-03-02 02:51:44 +0100

    feat(eslint): modernize parser-options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  ignorePatterns: ['!.*']
+  ignorePatterns: ['!.*'],
+
+  // REF:
+  //    https://eslint.org/docs/latest/use/configure/language-options#specifying-parser-options
+  //    https://stackoverflow.com/questions/42706584/eslint-error-parsing-error-the-keyword-const-is-reserved
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  }
 }
